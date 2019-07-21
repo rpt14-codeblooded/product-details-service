e8c6f9e050468b695a134189f1bab7ccf329345d
creationator <morton.derrick@gmail.com>
2019-07-20 22:24:35 -0400

    Updated environment variable

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./index').app;
 
-var port =  app.set('port', process.env.PORT || 3002);
+var port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 })
